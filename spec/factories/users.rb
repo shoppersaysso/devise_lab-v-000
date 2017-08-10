328e3565fe72bc897aa63a85f05d6335b9138b7d
Sara Hopper <sara.hopper@gmail.com>
2017-08-10 19:07:08 +0000

    automatically backed up by learn

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    name      "That OneGuy"
+    email     "thatguy@yeah.com"
+    password  "sauceboss"
+    password_confirmation   "sauceboss"
   end
 end
