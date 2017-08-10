be45b585d3ebea998db36dc6ba08e06394c86277
Sara Hopper <sara.hopper@gmail.com>
2017-08-10 17:58:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
      user.password = Devise.friendly_token[0,20]
    end
  end
- 
+
 end
